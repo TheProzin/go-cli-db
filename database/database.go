3a9723b5c748584e4cc7c2aee2bea210cff56528
Fábio Telles Justino <tellesjustinofabio@gmail.com>
2026-02-27 20:15:48 -0300

    Option to restore database

--- database/database.go
+++ database/database.go
@@ -3,30 +3,30 @@ package database
 import (
 	"fmt"
 	"go-cli-db/globals"
-	"log"
 	"os"
 	"os/exec"
 	"time"
 )
 
 type DatabaseData struct {
-	Password     string   `json:"db_password"`
-	Username     string   `json:"db_username"`
-	Host         string   `json:"db_host"`
-	Name         string   `json:"db_name"`
+	ConfigName   string   `json:"config_name"`
+	DbUsername   string   `json:"db_username"`
+	DbPassword   string   `json:"db_password"`
+	DbHost       string   `json:"db_host"`
+	DbName       string   `json:"db_name"`
 	IgnoreTables []string `json:"ignore_tables"`
 }
 
-func InitDbDump(dbConfig DatabaseData) {
+func InitDbDump(dbConfig DatabaseData) (string, error) {
 	time := time.Now().Format("20060102150405")
-	outputFile := dbConfig.Name + "__t" + time + ".sql"
+	outputFile := dbConfig.DbName + "__t" + time + ".sql"
 	args := []string{
 		"--max_allowed_packet=512M",
 		"--skip-lock-tables",
 		"--skip-add-locks",
-		"-h", dbConfig.Host,
-		"-u", dbConfig.Username,
-		"-p" + dbConfig.Password,
+		"-h", dbConfig.DbHost,
+		"-u", dbConfig.DbUsername,
+		"-p" + dbConfig.DbPassword,
 		"--routines",
 		"--triggers",
 		"--set-charset",
@@ -35,33 +35,72 @@ func InitDbDump(dbConfig DatabaseData) {
 	}
 
 	for _, table := range dbConfig.IgnoreTables {
-		args = append(args, "--ignore-table="+dbConfig.Name+"."+table)
+		args = append(args, "--ignore-table="+dbConfig.DbName+"."+table)
 	}
 
-	args = append(args, dbConfig.Name)
+	args = append(args, dbConfig.DbName)
 
 	cmd := exec.Command("mysqldump", args...)
 
-	dbFilesDir := globals.GoDotEnvVariable("DB_BACKUP_DIR")
-	if _, err := os.Stat(dbFilesDir); err != nil {
-		err := os.Mkdir(dbFilesDir, 0750)
+	dbBkpFilesDir := globals.GoDotEnvVariable("DB_BACKUP_DIR")
+	if _, err := os.Stat(dbBkpFilesDir); err != nil {
+		err := os.Mkdir(dbBkpFilesDir, 0750)
 		if err != nil && !os.IsExist(err) {
-			log.Fatal(err)
+			fmt.Fprintln(os.Stderr, "Error creating file:", err)
+			return "", err
 		}
 	}
 
-	outFile, err := os.Create(dbFilesDir + outputFile)
+	outFile, err := os.Create(dbBkpFilesDir + outputFile)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error creating file:", err)
-		return
+		return "", err
 	}
 	defer outFile.Close()
 
 	cmd.Stdout = outFile
 	cmd.Stderr = os.Stderr
 
+	fmt.Fprintln(os.Stdout, "Running mysqldump...")
 	err = cmd.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
+		return "", err
+	}
+
+	fmt.Fprintln(os.Stdout, "Success on making backup")
+	return outputFile, nil
+}
+
+func InitDbRestore(dumpFileName string, dbConfig DatabaseData) {
+	args := []string{
+		"--max_allowed_packet=1024M",
+		"--quick",
+		"-h", dbConfig.DbHost,
+		"-u", dbConfig.DbUsername,
+		"-p" + dbConfig.DbPassword,
+		dbConfig.DbName,
+	}
+
+	cmd := exec.Command("mysql", args...)
+
+	dbBkpFilesDir := globals.GoDotEnvVariable("DB_BACKUP_DIR")
+	inFile, err := os.Open(dbBkpFilesDir + dumpFileName)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error opening dump file:", err)
+		return
+	}
+	defer inFile.Close()
+
+	cmd.Stdin = inFile
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Fprintln(os.Stdout, "Running mysql restore...")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+	} else {
+		fmt.Fprintln(os.Stdout, "Success on restore")
 	}
 }
--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"go-cli-db/globals"
 	"go-cli-db/prompts"
 	"go-cli-db/select_option"
-	"log"
 	"os"
 	"os/signal"
 	"strings"
@@ -63,43 +62,220 @@ func ShowMainMenu() {
 		Id    int
 		Label string
 	}{Id: 2, Label: "Start backup"})
+	mainMenu = append(mainMenu, struct {
+		Id    int
+		Label string
+	}{Id: 3, Label: "Restore dump"})
+	mainMenu = append(mainMenu, struct {
+		Id    int
+		Label string
+	}{Id: 9, Label: "Quit"})
 
-	mainMenuOption, err := prompts.SelectPrompt("Choose your option:", mainMenu)
+loop:
+	for {
+		fmt.Print(globals.ResetTerminal)
+		mainMenuOption, err := prompts.SelectPrompt("Choose your option:", mainMenu)
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error: "+err.Error())
-	}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error: "+err.Error())
+			break loop
+		}
 
-	if mainMenuOption == 1 {
-		CreateBackup()
+		switch mainMenuOption {
+		case 1:
+			CreateBackup()
+		case 2:
+			ChooseDbFileForBackup()
+		case 3:
+			ChooseDbFileForRestore()
+		case 9:
+			break loop
+		}
 	}
 }
 
 func CreateBackup() {
 
-	var Database database.DatabaseData
-	Database.Host = prompts.StringPrompt("Database Host:", false)
-	Database.Name = prompts.StringPrompt("Database Name:", false)
-	Database.Username = prompts.StringPrompt("Database Username:", false)
-	Database.Password = prompts.PasswordPrompt("Database Password:")
+	var dbConfig database.DatabaseData
+	dbConfig.ConfigName = prompts.StringPrompt("Configuration name:", false)
+	dbConfig.DbHost = prompts.StringPrompt("Database Host:", false)
+	dbConfig.DbName = prompts.StringPrompt("Database Name:", false)
+	dbConfig.DbUsername = prompts.StringPrompt("Database Username:", false)
+	dbConfig.DbPassword = prompts.PasswordPrompt("Database Password:")
 	ignoreTables := prompts.StringPrompt("Database Ignore Tables (separated by comma):", true)
-	Database.IgnoreTables = strings.Split(ignoreTables, ",")
+	dbConfig.IgnoreTables = strings.Split(ignoreTables, ",")
 	fmt.Print(globals.ResetTerminal)
 
-	jsonDatabase, err := json.Marshal(Database)
+	jsonDatabase, err := json.Marshal(dbConfig)
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return
 	}
 
 	dbFilesDir := globals.GoDotEnvVariable("DB_FILES_DIR")
 	if _, err := os.Stat(dbFilesDir); err != nil {
 		err := os.Mkdir(dbFilesDir, 0750)
 		if err != nil && !os.IsExist(err) {
-			log.Fatal(err)
+			fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+			return
 		}
 	}
-	err = os.WriteFile(dbFilesDir+Database.Name+".json", jsonDatabase, 0644)
 
-	database.InitDbDump(Database)
+	err = os.WriteFile(dbFilesDir+dbConfig.ConfigName+".json", jsonDatabase, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return
+	}
+
+	startBkp := prompts.YesOrNoPrompt("Start backup?", true)
+	if startBkp {
+		sqlDumpFileName, err := database.InitDbDump(dbConfig)
+		if err != nil {
+			return
+		}
+		restoreFile := prompts.YesOrNoPrompt("Do you want to restore the file?", true)
+		if restoreFile {
+			dbConfig, err := chooseDbConfig()
+			if err != nil {
+				return
+			}
+
+			database.InitDbRestore(sqlDumpFileName, dbConfig)
+		}
+	}
+}
+
+func ChooseDbFileForBackup() {
+
+	dbConfig, err := chooseDbConfig()
+	if err != nil {
+		return
+	}
+
+	startBkp := prompts.YesOrNoPrompt("Start backup?", true)
+	if startBkp {
+		sqlDumpFileName, err := database.InitDbDump(dbConfig)
+		if err != nil {
+			return
+		}
+		restoreFile := prompts.YesOrNoPrompt("Do you want to restore the file?", true)
+		if restoreFile {
+			dbConfig, err := chooseDbConfig()
+			if err != nil {
+				return
+			}
+
+			database.InitDbRestore(sqlDumpFileName, dbConfig)
+		}
+	}
+}
+
+func ChooseDbFileForRestore() {
+
+	dbConfig, err := chooseDbConfig()
+	if err != nil {
+		return
+	}
+
+	sqlDumpFileName, err := chooseDumpFileName()
+	if err != nil {
+		return
+	}
+
+	fmt.Fprintf(os.Stdout, "Restoring from: %s \nTo db: %s with host: %s\n", sqlDumpFileName, dbConfig.DbName, dbConfig.DbHost)
+	startBkp := prompts.YesOrNoPrompt("Start backup?", true)
+	if startBkp {
+		database.InitDbRestore(sqlDumpFileName, dbConfig)
+	}
+}
+
+func chooseDbConfig() (database.DatabaseData, error) {
+
+	dbConfigFilesDir := globals.GoDotEnvVariable("DB_FILES_DIR")
+	files, err := os.ReadDir(dbConfigFilesDir)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return database.DatabaseData{}, err
+	}
+
+	if len(files) == 0 {
+		fmt.Fprintln(os.Stderr, "Don't exist any config file")
+		return database.DatabaseData{}, err
+	}
+
+	var filesOptions select_option.Options
+	for i, file := range files {
+		filesOptions = append(filesOptions, struct {
+			Id    int
+			Label string
+		}{
+			Id:    i,
+			Label: file.Name(),
+		})
+	}
+
+	fileDbSelected, err := prompts.SelectPrompt("Choose your db config:", filesOptions)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return database.DatabaseData{}, err
+	}
+
+	configDbJson := files[fileDbSelected]
+
+	file, err := os.Open(dbConfigFilesDir + configDbJson.Name())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return database.DatabaseData{}, err
+	}
+	defer file.Close()
+
+	var dbConfig database.DatabaseData
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&dbConfig); err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return database.DatabaseData{}, err
+	}
+
+	return dbConfig, nil
+}
+
+func chooseDumpFileName() (string, error) {
+
+	dbBkpFilesDir := globals.GoDotEnvVariable("DB_BACKUP_DIR")
+	filesFrom, err := os.ReadDir(dbBkpFilesDir)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return "", err
+	}
+
+	if len(filesFrom) == 0 {
+		fmt.Fprintln(os.Stderr, "Don't exist any dump file")
+		return "", err
+	}
+
+	var filesFromOptions select_option.Options
+	for i, file := range filesFrom {
+		filesFromOptions = append(filesFromOptions, struct {
+			Id    int
+			Label string
+		}{
+			Id:    i,
+			Label: file.Name(),
+		})
+	}
+
+	fileWhereFromDbSelected, err := prompts.SelectPrompt("Choose your sql dump:", filesFromOptions)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err.Error())
+		return "", err
+	}
+
+	sqlDumpFile := filesFrom[fileWhereFromDbSelected]
+
+	return sqlDumpFile.Name(), nil
 }
